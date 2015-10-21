864ecb990cf82b30daeba637381249f43f1de6c4
PalomaPG <paloma.perez.gar@gmail.com>
2015-10-21 13:50:54 -0300

    new changes

--- scripts/plot_params.py
+++ scripts/plot_params.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python
-
+# -*- coding: utf-8 -*-
 import math
 import scipy as sc
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib as mpl
 
-logs_ = "/home/amalia/Test/gim2d/logs"
-iters_=[1,10,100, 250,500,1000, 1500,2000,50000,10000]
-mdpar_d = {0:'Flux', 1:'B/T', 2:'bulge_Re', 3:'bulge_e', 4:'bulge_phi', 5:'exp_disc_scale', 6:'disc_incl_i', 7:'disk_phi', 8:'sp_x_gal', 9:'sp_y_gal', 10:'bkg', 11:'bulge_Sersic_index'}
+
+
+
+pgf_with_pdflatex = {
+    "pgf.texsystem": "pdflatex",
+    "pgf.preamble": [
+         r"\usepackage[utf8x]{inputenc}",
+         r"\usepackage[T1]{fontenc}",
+         r"\usepackage{cmbright}",
+         ]
+}
+
+mpl.rcParams.update(pgf_with_pdflatex)
+
+logs = "/home/amalia/Tests/gim2d/gim2d-test/inputs/logs"
+mpl.use("pgf")
+iters=[100, 10000,1000, 10, 1500, 1, 2000, 250, 5000, 500]
+mdpar_d = {0:"Flux", 1:"B_T", 2:'bulge_Re', 3:'bulge_e', 4:'bulge_phi', 5:'exp_disc_scale', 6:'disc_incl_i', 7:'disk_phi', 8:'sp_x_gal', 9:'sp_y_gal', 10:'bkg', 11:'bulge_Sersic_index'}
 '''
 opens and reads a log file 
 '''
@@ -21,7 +36,7 @@ def readlog(filename):
 				values.append(data[1:])
 	return values
 
-def readLogs(filename, iters):
+def readLogs(filename, it):
 	pairs = []
 	i = 0
 	with open(filename) as f:
@@ -50,14 +65,19 @@ def par_val(pairs, param_n):
 	p_vals.append(max_)
 	p_vals.append(param_n)
 
-	return param_n
+	return p_vals
 
 def main():
 
-	pairs = readLogs()
-	par_val(pairs, 0)
+	pairs = readLogs(logs, iters)
+	for k in mdpar_d.keys():
+		p_vals = par_val(pairs, k)
+		pl = Plotter(p_vals[0], p_vals[1], p_vals[2], p_vals[3], mdpar_d[k])
+		pl.make_graph()
+	#for p in pairs:
+	#	print p[1]
 	
-
+import matplotlib.pyplot as plt
 
 class Plotter(object):
 
@@ -69,13 +89,28 @@ class Plotter(object):
 		'''
 		self.x = x
 		self.y = y
-		self.min = min_
-		self.max = max_
+		self.min_ = min_
+		self.max_ = max_
+		self.pname = param_name
  
 	def make_graph(self):
 		fig = plt.figure()
 		ax = fig.add_subplot(1, 1, 1)
-		plt.errorbar(x, y, xerr=0, yerr=[y-min_, max_-y], fmt ='o', color='blue')
-		ax.set_title('Iteration vs'+ param_name)
-		plt.show()
+		ax.set_xlabel(u"Iterations")
+		ax.set_ylabel(self.pname)
+		y=np.asarray(self.y, np.float)
+		min_=np.asarray(self.min_, np.float)
+		max_=np.asarray(self.max_, np.float) 
+		x=np.asarray(self.x, np.float)
+		y_min = min(y-y*0.5)#min(min_)
+		y_max=max(y+y*0.5)#max(max_)
+		plt.ylim((y_min, y_max))
+		plt.errorbar(x, y, xerr=0, yerr=[y-min_,max_-y], fmt ='o', color='black')
+		if self.pname == "B/T":
+			ax.set_title('Iteration vs '+ u"B/T")
+		else:
+			ax.set_title('Iteration vs '+ self.pname)
+		plt.savefig(self.pname+'.pdf')
+		plt.close()
+		#plt.show()
 
